823810c0313a5a71d575c8122a134d1d2b385263
André Ramaldes <67028873+Andre07-creator@users.noreply.github.com>
2021-11-12 19:36:25 -0300

    update v1.1

--- teste/Program.cs
+++ teste/Program.cs
@@ -6,6 +6,8 @@ namespace teste
     {
         static void Main(string[] args)
         {
+            int num = 2;
+            double num1 = 4.5;
             Console.WriteLine("Hello World!");
         }
     }
